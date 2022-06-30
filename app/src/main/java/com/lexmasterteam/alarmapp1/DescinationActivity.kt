ce8b23e7028f1e5e2789da86900fae976f8e53c1
NUREKK2003 <jakubn20033002@gmail.com>
2022-06-30 09:35:24 +0200

    initial

---
+++ app/src/main/java/com/lexmasterteam/alarmapp1/DescinationActivity.kt
@@ -0,0 +1,11 @@
+package com.lexmasterteam.alarmapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DescinationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_descination)
+    }
+}
